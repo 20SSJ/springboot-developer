1df9407d2cd5815ee562fe2b54477070b258ad61
20SSJ <ssjjnn01@gachon.ac.kr>
2025-07-31 14:05:12 +0900

    validation

--- src/main/java/me/sangjunsuh/springbootdeveloper/dto/AddArticleRequest.java
+++ src/main/java/me/sangjunsuh/springbootdeveloper/dto/AddArticleRequest.java
@@ -1,19 +1,25 @@
 package me.sangjunsuh.springbootdeveloper.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import me.sangjunsuh.springbootdeveloper.domain.Article;
 
-@NoArgsConstructor  // 기본 생성자 추가
-@AllArgsConstructor // 모든 필드 값을 파라미터로 받는 생성자 추가
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 public class AddArticleRequest {
 
+    @NotNull
+    @Size(min = 1, max = 10)
     private String title;
+
+    @NotNull
     private String content;
 
-    public Article toEntity(String author) { // 생성자를 사용해 객체 생성
+    public Article toEntity(String author) {
         return Article.builder()
                 .title(title)
                 .content(content)
